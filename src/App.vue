2c32af3d822915422467dcadae6d76f25033768a
EroTiXx <alexis.martineau.pro@gmail.com>
2018-10-03 13:23:47 +0200

    Change gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <axmVueAnimationNumber number="1234.67" customclass=""></axmVueAnimationNumber>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import axmVueAnimationNumber from './components/axmVueAnimationNumber.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            axmVueAnimationNumber
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/axmVueAnimationNumber.vue
@@ -0,0 +1,68 @@
+<template>
+    <div :class="customclass">{{ displayNumber(valueAnimate) }}</div>
+</template>
+
+<script>
+    export default {
+        name: 'axmVueAnimationNumber',
+        props: ['number', 'customclass'],
+        data: function () {
+            return {
+                value: null, // Your integer number
+                valueAnimate: 0, // Don't touch
+                duration: 5000 // Duration of animation in ms
+            }
+        },
+        mounted: function () {
+            this.value = this.number;
+            if(this.value != null && this.value != '' && this.value != undefined){
+
+                this.value = parseFloat(this.value);
+
+                if(this.value != NaN){
+
+                    let that = this;
+                    let myTimer
+                    let delay = 9;
+                    let adition = 1;
+
+                    if(this.value < 0){
+                        this.isNegative = true;
+                    }
+
+                    if(this.duration > 0){
+                        var tmpDelay = this.duration / delay;
+                        adition = Math.abs(this.value) / tmpDelay;
+                    }else{
+                        delay = 0;
+                    }
+
+                    myTimer = setInterval(function(){
+                        if((that.valueAnimate+=adition) < Math.abs(that.value)){
+                            that.valueAnimate += adition;
+                        }else{
+                            clearInterval(myTimer)
+                            that.valueAnimate = Math.abs(that.value);
+                        }
+                    }, delay);
+                }
+            }
+        },
+        methods: {
+            countDecimals: function (value) {
+                if (Math.floor(value) === value) {
+                    return 0;
+                }
+                return value.toString().split(".")[1].length || 0;
+            },
+            displayNumber: function (value) {
+                if (value != null && value != '' && value != undefined && value != NaN) {
+                    return (this.isNegative ? '-' : '') + value.toFixed(this.countDecimals(this.value));
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
